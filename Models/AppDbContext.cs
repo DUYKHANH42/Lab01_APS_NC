9ee1c84ff9d794382693165a7b5572fa1efba5b1
DUYKHANH42 <kxtduykhanh@gmail.com>
2026-04-21 15:32:24 +0700

    demo EFcore

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Lab01.Models
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+    }
+}
